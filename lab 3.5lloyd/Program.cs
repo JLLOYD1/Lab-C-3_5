59ac1fdd2a70973f9449515492f0ea70869ad786
JLLOYD1 <61300197+JLLOYD1@users.noreply.github.com>
2020-02-20 19:17:25 -0600

    Add files via upload

---
+++ lab 3.5lloyd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab_3._5lloyd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = " MILF";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord)) 
+            {
+                isSpam = true;
+                Console.WriteLine("the message is spam");
+            }
+            
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("the message is not spam.");
+            }
+
+        }
+    }
+}
+
